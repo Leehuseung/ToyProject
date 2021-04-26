07c6a4979a8ee162baa9617c7b77e2b830c6aa96
hsLee <gntmddl4261@gmail.com>
2021-04-27 00:02:12 +0900

    - App.js 기본 select 데이터 출력

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [testList, setList] = useState([]);
+
+    let dataList;
+
+    useEffect(() => {
+        fetch('http://localhost:8080/select')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setList(result)
+                }
+            );
+    });
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a>select * from test</a>
+                <a>
+                    {
+                        testList.map(test => (
+                            <a>{test.test},</a>
+                        ))
+                    }
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
