beb6a958b5adb55d08dd710d8f49f21dab86cb1f
hsLee <gntmddl4261@gmail.com>
2021-04-29 06:19:58 +0900

    - db 구현 - npm test nodemon 추가

--- DATABASE/DDL.sql
+++ DATABASE/DDL.sql
@@ -1,3 +1,9 @@
 create table TEST (
     test varchar(200)
 );
+CREATE TABLE todo (
+    id varchar(200) DEFAULT NULL,
+    text varchar(200) DEFAULT NULL,
+    isCompleted tinyint(1) DEFAULT NULL
+);
+
--- servers/apollo_server.js
+++ servers/apollo_server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const {bodyParserGraphQL} = require('body-parser-graphql');
 const compression = require('compression');
+const config = require('../config.json');
+const mysql = require('mysql2/promise');
+
+mysql.pool = mysql.createPool({
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.database
+});
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -35,23 +44,43 @@ const id = () => {
     return Math.random().toString(36).substring(10);
 };
 
+const pool = {
+    query: async (query, value) => {
+        try {
+            var connection = await mysql.pool.getConnection(async conn => conn);
+            let [result] = value
+                ? await connection.query(query, value)
+                : (await connection.query(query)) || null;
+            connection.release();
+            return result;
+        } catch (err) {
+            console.log(err);
+            connection.rollback(() => {
+            });
+        }
+    },
+};
 
-let todos = [
-    {
-        id: id(),
-        text: 'Todo Sample 1',
-        isCompleted: false,
+
+let todos = {
+    get: () => {
+        return pool.query('SELECT * FROM TODO');
     },
-    {
-        id: id(),
-        text: 'Todo Sample 2',
-        isCompleted: false,
+    delete: (id) => {
+        pool.query('DELETE FROM TODO WHERE ID = ?', [id]);
     },
-];
+    add: (todo) => {
+        pool.query('INSERT INTO TODO (ID,TEXT,ISCOMPLETED) VALUES(?,?,?)', [todo.id, todo.text, todo.isCompleted]);
+    },
+    update: (todo) => {
+        pool.query('UPDATE TODO SET ISCOMPLETED = ? WHERE ID = ?', [todo.isCompleted, todo.id]);
+    }
+}
+
 
 const resolvers = {
     Query: {
-        todos: () => todos,
+        todos: () => todos.get(),
     },
     Mutation: {
         addTodo: (_, {text}) => {
@@ -60,24 +89,23 @@ const resolvers = {
                 text: text,
                 isCompleted: false,
             };
-            todos.push(todo);
+            todos.add(todo);
             return todo;
         },
         updateTodo: (_, args) => {
             const todo = {
-                id : args.id,
-                text : args.text,
+                id: args.id,
+                text: args.text,
                 isCompleted: args.isCompleted,
             };
-            todos = todos.map(t => t.id === args.id ? todo : t);
+            todos.update(todo);
             return todo;
         },
         deleteTodo: (_, {id}) => {
-            todos = todos.filter((t) => t.id !== id);
+            todos.delete(id);
         }
     }
 };
-
 
 
 const port = 5050;
