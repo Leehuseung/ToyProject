d9508d3acd9e8af343e7ee3938f447bce9505b0a
hyobbb <yang4h0b1>
2021-05-06 11:03:31 +0900

    dot-env implement (parcel bundling removed)

--- servers/database.js
+++ servers/database.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config()
 
 const pool = mysql.createPool({
-    host: process.env.server_host,
-    user: process.env.db_user,
-    password: process.env.db_password,
-    database: process.env.database
+    host: process.env.SERVER_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
 });
 
 module.exports = {
