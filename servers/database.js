8dcb070043e0ab50ff4bd8c1503576d281e55a66
hyobbb <yang4h0b1>
2021-05-06 10:18:16 +0900

    dot-env implement (parcel bundling removed)

--- servers/database.js
+++ servers/database.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2/promise');
-const config = require('../config.json');
 
 const pool = mysql.createPool({
-    host: config.database.host,
-    user: config.database.user,
-    password: config.database.password,
-    database: config.database.database
+    host: process.env.server_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.database
 });
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
-import App from './App';
-
+import Sidebar from "./component/Sidebar";
+import Main from "./component/Main";
 
 const client = new ApolloClient({
         uri: `${window.location.protocol}//${window.location.hostname}:5050/gql`,
@@ -14,7 +14,10 @@ const client = new ApolloClient({
 ReactDOM.render(
     <ApolloProvider client={client}>
         <React.StrictMode>
-            <App/>
+            <div className="App">
+                <Sidebar/>
+                <Main/>
+            </div>
         </React.StrictMode>
     </ApolloProvider>,
     document.getElementById('root')
